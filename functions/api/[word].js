6061665a90083cc70a69546017872b771fc4faa1
luisagd <luisaguero011@gmail.com>
2024-08-22 15:03:27 -0400

    Testing cloudfare functions 2.98 - Changed fecthData

--- functions/api/[word].js
+++ functions/api/[word].js
@@ -4,7 +4,11 @@ const dictionary = require("./json/diccionario.json");
 const fetchData = (word) => {
   word = word.toLowerCase().replaceAll("'", "’").replaceAll("´", "’");
   const result = fuzzysort.go(word, dictionary, { key: "word", limit: 5 });
-  return result;
+  let results = []
+  for (let i = 0; i < result.length; i++) {
+    results.push( result[i].obj);
+  }
+  return results;
 };
 export function onRequest(context) {
     let res = new Response(fetchData(context.params.word))
