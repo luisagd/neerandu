b18f2cfee3417b07f9b14ae76296b4309cfc9b26
luisagd <luisaguero011@gmail.com>
2024-08-22 13:14:30 -0400

    Testing cloudfare functions 2.75

--- functions/api/[word].js
+++ functions/api/[word].js
@@ -1,3 +1,11 @@
+import fuzzysort from "fuzzysort";
+const dictionary = require("../json/diccionario.json");
+
+const fetchData = (word) => {
+  word = word.toLowerCase().replaceAll("'", "’").replaceAll("´", "’");
+  const result = fuzzysort.go(word, dictionary, { key: "word", limit: 5 });
+  return result;
+};
 export function onRequest(context) {
-    return new Response("Hello, world!"+context.params.word)
+    return new Response(fetchData(context.params.word))
   }
