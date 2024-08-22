20e252e76261a0bfc9be839dd33b5e3e0e35b387
luisagd <luisaguero011@gmail.com>
2024-08-22 15:14:19 -0400

    Testing cloudfare functions 3

--- functions/api/[word].js
+++ functions/api/[word].js
@@ -8,7 +8,7 @@ const fetchData = (word) => {
   for (let i = 0; i < result.length; i++) {
     results.push( result[i].obj);
   }
-  return results;
+  return result;
 };
 export function onRequest(context) {
     let res = new Response(fetchData(context.params.word))
