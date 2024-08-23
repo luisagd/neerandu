51b807a621e481b7adcafae50cc4da3fc05302e8
luisagd <luisaguero011@gmail.com>
2024-08-23 17:37:45 -0400

    Fixed issue with worker pages. Moved diccionario to pages instead

--- functions/api.js
+++ functions/api.js
@@ -1,19 +1,10 @@
 import fuzzysort from "fuzzysort";
-const dictionary = require("./json/diccionario.json");
 
 
-const fetchData = (word) => {
-  word = word.toLowerCase().replaceAll("'", "’").replaceAll("´", "’");
-  const result = fuzzysort.go(word, dictionary, { key: "word", limit: 5 });
-  let results = []
-  for (let i = 0; i < result.length; i++) {
-    results.push(result[i].obj);
-    //{"word":result[i].obj.word, "meaning": result[i].obj.meaning} 
-  }
-  return JSON.stringify(result);
-};
 
-export function onRequest(context) {
+
+export async function onRequest(context) {
+
   if (context.request.method === 'OPTIONS') {
     return new Response(null, {
       status: 204,
@@ -24,7 +15,22 @@ export function onRequest(context) {
       }
     });
   }
-    const url = new URL(context.request.url)
+    const url = new URL(context.request.url);
+    const url_dict = url.protocol+url.host+'/json/diccionario.json';
+    console.log(url_dict)
+    const resp = await context.env.ASSETS.fetch(url_dict);
+    const dictionary = await resp.json();
+    const fetchData = (word) => {
+      word = word.toLowerCase().replaceAll("'", "’").replaceAll("´", "’");
+      const result = fuzzysort.go(word, dictionary, { key: "word", limit: 5 });
+      let results = []
+      for (let i = 0; i < result.length; i++) {
+        results.push(result[i].obj);
+        //{"word":result[i].obj.word, "meaning": result[i].obj.meaning} 
+      }
+      return JSON.stringify(result);
+    };
+
     let query = url.searchParams.get("q");
 
     let res = new Response(fetchData(query));
